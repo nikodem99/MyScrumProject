af1863904fc88f88e31e5a12bc9cb5a7bab2856c
Nikodem <cwikla.nikodem@gmail.com>
2023-02-24 00:37:51 +0100

    commit
    commit

---
+++ development/js/index.js
@@ -0,0 +1,1 @@
+$.import_js('./carousel.js')
---
+++ dist/index.d1bc7d09.js
@@ -0,0 +1,36 @@
+let sliderImages = document.querySelectorAll(".slide");
+let arrowRight = document.querySelector("#arrow-right");
+let arrowLeft = document.querySelector("#arrow-left");
+let current = 0;
+function reset() {
+    for(let i = 0; i < sliderImages.length; i++)sliderImages[i].style.display = "none";
+}
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = "block";
+}
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = "block";
+    current--;
+}
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = "block";
+    current++;
+}
+arrowLeft.addEventListener("click", function() {
+    if (current === 0) current = sliderImages.length;
+    slideLeft();
+});
+arrowRight.addEventListener("click", function() {
+    if (current === sliderImages.length - 1) current = -1;
+    slideRight();
+});
+setInterval(function() {
+    if (current === sliderImages.length - 1) current = -1;
+    slideRight();
+}, 4000);
+startSlide();
+
+//# sourceMappingURL=index.d1bc7d09.js.map
